87a2b5eebc426d0b712acb0af065dd26f3091778
Xymdyx <samford892@gmail.com>
2022-02-28 18:00:45 -0500

    Implemented basic supersampling.

--- RayTracer-App/Camera/Camera.cs
+++ RayTracer-App/Camera/Camera.cs
@@ -86,7 +86,43 @@ namespace RayTracer_App.Camera
 			
 		}
 
-		// TODO runs tone reproduction on the irradiance triplet retrieved from an intersection
+		//attempt at supersampling with only doing 4 basic corners of a pixel
+		public Color superSamplePixel( Point centerPoint, float pixHeight, float pixWidth, World.World world )
+		{
+			Color averageHitColor = null;
+			Color[] hitColors = new Color[4]; //for super-sampling
+			Point[] hitPoints = new Point[4] {
+				new Point( centerPoint.x - pixWidth / 2, centerPoint.y + pixHeight / 2, centerPoint.z), //top-left
+				new Point( centerPoint.x + pixWidth / 2, centerPoint.y + pixHeight / 2, centerPoint.z ), //top-right
+				new Point( centerPoint.x - pixWidth / 2, centerPoint.y - pixHeight / 2, centerPoint.z ), //bottom-left
+				new Point( centerPoint.x + pixWidth /2, centerPoint.y - pixHeight / 2, centerPoint.z ), //bottom-right
+			};
+			
+			for( int hitIdx = 0; hitIdx < 4; hitIdx ++ )
+			{
+				LightRay hitRay = new LightRay( hitPoints[hitIdx] - this.eyePoint, this.eyePoint );
+				hitColors[hitIdx] = world.spawnRay( hitRay );
+			}
+
+			//average the 4 colors
+			for (int colNum = 0; colNum < 4; colNum++)
+			{
+				if (hitColors[colNum] != null)
+				{
+					if (averageHitColor == null)
+						averageHitColor = hitColors[colNum];
+					else
+						averageHitColor += hitColors[colNum];
+				}
+			}
+
+			if ( averageHitColor != null )
+				averageHitColor = averageHitColor.scale( .25f );
+
+			return averageHitColor;
+		}
+
+		// runs tone reproduction on the irradiance triplet retrieved from an intersection
 		// just ternaries for now. If the sum of energy is >1, we max it at 1.
 		public Color runTR( Color irradiance ) 
 		{
@@ -97,6 +133,7 @@ namespace RayTracer_App.Camera
 
 			return trColor;
 		}
+
 
 //tried list of float[] and float[]...
 		public byte[] render( World.World world, int imageHeight, int imageWidth, float focalLen )
@@ -132,8 +169,8 @@ namespace RayTracer_App.Camera
 			Point fpPoint = new Point ( (-fpWidth / 2) + (pixWidth / 2), (fpHeight / 2) - (pixHeight / 2), focalLen); //gldrawPixels starts drawing lower-left corner at raster positions
 			LightRay fire = new LightRay( fpPoint - this.eyePoint , this.eyePoint );
 			Color hitColor = null;
-			Color[] hitColors = new Color[5]; //for super-sampling
 			byte[] hitColorArr = null;
+			bool isSuperSampling = false;
 
 
 			int hits = 0;
@@ -141,9 +178,14 @@ namespace RayTracer_App.Camera
 			{
 				for ( int x = 0; x < imageWidth; x++)
 				{
-					//supersample here... have an array of hitcolors... average them then pass to TR below
-					fire.direction = fpPoint - this.eyePoint;
-					hitColor = world.spawnRay( fire ); //this will be irradiance....
+					//supersample branch here... have an array of hitcolors... average them then pass to TR below
+					if (!isSuperSampling)
+					{
+						fire.direction = fpPoint - this.eyePoint;
+						hitColor = world.spawnRay( fire ); //this will be irradiance....
+					}
+					else
+						hitColor = superSamplePixel( fpPoint, pixHeight, pixWidth, world );
 
 					if (hitColor != null)
 					{
